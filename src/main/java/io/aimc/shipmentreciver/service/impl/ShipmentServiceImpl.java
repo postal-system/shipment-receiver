13fd3f86d6143354c5f1bd88479738122acf6090
Angelina Andreychenko <aandreychenko@codero.io>
2021-12-09 18:24:06 +0600

    added feign exception handling

--- src/main/java/io/aimc/shipmentreciver/service/impl/ShipmentServiceImpl.java
+++ src/main/java/io/aimc/shipmentreciver/service/impl/ShipmentServiceImpl.java
@@ -1,5 +1,6 @@
 package io.aimc.shipmentreciver.service.impl;
 
+import feign.FeignException;
 import io.aimc.shipmentreciver.client.ReceiverClient;
 import io.aimc.shipmentreciver.entity.Shipment;
 import io.aimc.shipmentreciver.model.Person;
@@ -7,9 +8,11 @@ import io.aimc.shipmentreciver.model.RawShipment;
 import io.aimc.shipmentreciver.repository.ShipmentRepository;
 import io.aimc.shipmentreciver.service.ShipmentService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class ShipmentServiceImpl implements ShipmentService {
     private final ReceiverClient receiverClient;
@@ -17,14 +20,18 @@ public class ShipmentServiceImpl implements ShipmentService {
 
     @Override
     public void add(RawShipment rawShipment) {
-        Person person = receiverClient.getById(rawShipment.getIdReceiver());//todo feign exception handler
-        Shipment shipment = Shipment.builder()
-                .sourceId(rawShipment.getSourceId())
-                .rawShipment(rawShipment)
-                .sender(rawShipment.getSender())
-                .content(rawShipment.getContent())
-                .fullName(person.getFirstName().concat(" ").concat(person.getLastName()).concat(" ").concat(person.getPatronymic()))
-                .build();
-        shipmentRepository.save(shipment);
+        try {
+            Person person = receiverClient.getById(rawShipment.getIdReceiver());
+            Shipment shipment = Shipment.builder()
+                    .sourceId(rawShipment.getSourceId())
+                    .rawShipment(rawShipment)
+                    .sender(rawShipment.getSender())
+                    .content(rawShipment.getContent())
+                    .fullName(person.getFirstName().concat(" ").concat(person.getLastName()).concat(" ").concat(person.getPatronymic()))
+                    .build();
+            shipmentRepository.save(shipment);
+        } catch (FeignException.NotFound e) {
+            log.error("person not found");
+        }
     }
 }
