126697e23cef16352854989e18180dc75f48e39a
Angelina Andreychenko <aandreychenko@codero.io>
2021-12-15 18:05:10 +0600

    add sender service

--- src/main/java/io/aimc/shipmentreciver/PortionSenderScheduler.java
+++ src/main/java/io/aimc/shipmentreciver/PortionSenderScheduler.java
@@ -1,26 +1,19 @@
 package io.aimc.shipmentreciver;
 
-import io.aimc.shipmentreciver.service.PortionService;
+import io.aimc.shipmentreciver.service.PortionSenderToRabbitService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
-@Slf4j
 @RequiredArgsConstructor
+@Slf4j
 public class PortionSenderScheduler {
-    private final PortionService portionService;
-    private final RabbitTemplate rabbitTemplate;
-
-    @Value("${spring.rabbitmq.queue}")
-    private String queue;
+    private final PortionSenderToRabbitService portionSenderToRabbitService;
 
     @Scheduled(fixedDelayString = "${scheduler.fixed-delay}")
     public void send() {
-        rabbitTemplate.convertAndSend(queue, portionService.get());
-        log.info("send to rabbit");
+        portionSenderToRabbitService.send();
     }
 }
