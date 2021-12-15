10e68adecae3ef951d57d79cba219bb97d4fdd2b
Angelina Andreychenko <aandreychenko@codero.io>
2021-12-15 18:04:40 +0600

    delete implement serializable

--- src/main/java/io/aimc/shipmentreciver/model/Portion.java
+++ src/main/java/io/aimc/shipmentreciver/model/Portion.java
@@ -1,15 +1,14 @@
 package io.aimc.shipmentreciver.model;
 
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.io.Serializable;
 import java.util.List;
 import java.util.UUID;
 
 @Data
-@Builder
-public class Portion implements Serializable {
+@AllArgsConstructor
+public class Portion {
     private UUID id;
     private List<UUID> shipmentIds;
 }
