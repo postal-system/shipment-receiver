df58c9d0b214bd604d9adc2e340da626c7977e63
Angelina Andreychenko <aandreychenko@codero.io>
2021-12-15 18:04:11 +0600

    rename receiver client

--- src/main/java/io/aimc/shipmentreciver/client/PersonClient.java
+++ src/main/java/io/aimc/shipmentreciver/client/PersonClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "${spring.feign.client.name}", url = "${spring.feign.client.url}")
-public interface ReceiverClient {
+public interface PersonClient {
     @GetMapping("/api/persons/{id}")
     Person getById(@PathVariable Integer id);
 }
